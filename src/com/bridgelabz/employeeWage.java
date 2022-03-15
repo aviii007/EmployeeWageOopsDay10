3a130fb3676ab614bcdd6e474d5e5b1736bb9f14
Ashvini Kanojia <kanojia.avi@gmail.com>
2022-03-15 13:12:28 +0530

    Adding the wage till condition has reached.

--- src/com/bridgelabz/employeeWage.java
+++ src/com/bridgelabz/employeeWage.java
@@ -8,10 +8,12 @@ public class employeeWage {
         int isPartTime=1;
         int isFullTime=2;
         int ratePerHour=20;
-        int days=20;
-        int hours=0,salary=0,totalSalary=0;
-        for(int counter=1;counter<=days;counter++)
+        int days=0,totalDays=20;
+        int maxHours=100;
+        int hours=0,totalHours=0,salary=0,totalSalary=0;
+        while(totalHours<maxHours && days<totalDays)
         {
+            totalDays+=1;
             int check=(int)Math.floor((Math.random()*10)%3);
             switch(check)
             {
@@ -25,11 +27,9 @@ public class employeeWage {
                     hours=0;
                     break;
             }
-            System.out.println(salary);
-            salary=hours*ratePerHour;
-            totalSalary+=salary;
+            totalHours+=hours;
         }
-        System.out.println("Salary of employee in a month is:"+totalSalary);
+        totalSalary=totalHours*ratePerHour;
+        System.out.println("Salary is:"+totalSalary);
     }
-
 }
