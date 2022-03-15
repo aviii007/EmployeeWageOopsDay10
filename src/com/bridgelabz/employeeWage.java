f23b90d83bdeb71a2682921cf4ab7010988fb4ea
Ashvini Kanojia <kanojia.avi@gmail.com>
2022-03-15 13:10:14 +0530

    Adding the wage for a month.

--- src/com/bridgelabz/employeeWage.java
+++ src/com/bridgelabz/employeeWage.java
@@ -8,22 +8,28 @@ public class employeeWage {
         int isPartTime=1;
         int isFullTime=2;
         int ratePerHour=20;
-        int hours=0,salary=0;
-        int check=(int)Math.floor((Math.random()*10)%3);
-        switch(check)
+        int days=20;
+        int hours=0,salary=0,totalSalary=0;
+        for(int counter=1;counter<=days;counter++)
         {
-            case 2:
-                hours=8;
-                break;
-            case 1:
-                hours=4;
-                break;
-            default:
-                hours=0;
-                break;
+            int check=(int)Math.floor((Math.random()*10)%3);
+            switch(check)
+            {
+                case 2:
+                    hours=8;
+                    break;
+                case 1:
+                    hours=4;
+                    break;
+                default:
+                    hours=0;
+                    break;
+            }
+            System.out.println(salary);
+            salary=hours*ratePerHour;
+            totalSalary+=salary;
         }
-        salary=hours*ratePerHour;
-        System.out.println("Salary is:"+salary);
+        System.out.println("Salary of employee in a month is:"+totalSalary);
     }
 
 }
