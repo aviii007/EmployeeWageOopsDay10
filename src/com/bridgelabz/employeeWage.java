8610c5f76a9f51268323d57d12abc73c889fd08b
Ashvini Kanojia <kanojia.avi@gmail.com>
2022-03-15 12:53:28 +0530

    Adding the Daily Wage.

--- src/com/bridgelabz/employeeWage.java
+++ src/com/bridgelabz/employeeWage.java
@@ -4,15 +4,16 @@ public class employeeWage {
 
     public static void main(String[] args) {
         System.out.println("Welcome to the Employee Wage Program");
+
         int isFullTime=1;
+        int ratePerHour=20;
+        int hours=0,salary=0;
         int check=(int)Math.floor((Math.random()*10)%2);
         if(check==isFullTime)
         {
-            System.out.println("Employee is present");
+            hours=8;
         }
-        else
-        {
-            System.out.println("Employee is absent");
-        }
+        salary=hours*ratePerHour;
+        System.out.println("Salary is:"+salary);
     }
 }
